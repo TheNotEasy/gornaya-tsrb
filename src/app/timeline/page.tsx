a2907746ba547cb0bdb577ba60704e8727283df8
TheNotEasy <sasha.antonov092@gmail.com>
2025-11-28 16:33:29 +0900

    completed timeline

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,19 +1,42 @@
 "use client";
 
 import Timeline from "@/src/components/timeline";
-import Image from "next/image";
 import { useState } from "react";
 
+import events from "./events.json";
+import Markdown from "react-markdown";
+
 export default function Home() {
-  const [stage, setStage] = useState(1);
+  const [stage, setStage] = useState(0);
 
   return (
-    <>
+    <div className="flex flex-col gap-12">
       <div className="flex flex-col align-center justify-center px-12 pt-12">
         <h1 className="text-center text-3xl">Таймлайн</h1>
-
+        {stage}
       </div>
-      <Timeline stage={stage}></Timeline>
-    </>
+      <div className="flex gap-5 mx-auto">
+        <button onClick={() => setStage(stage - 1)} className="">
+          Назад
+        </button>
+        <button onClick={() => setStage(stage + 1)} className="">
+          Вперед
+        </button>
+      </div>
+      <Timeline stage={stage}>
+        {events.map((value, idx) => (
+          <div className="flex flex-col items-center gap-5 w-lg" key={idx}>
+            <h1 className="font-bold">{value.title}</h1>
+            {value.image && <img src={value.image} alt={value.alt}></img>}
+            {value.description && <p>{value.description}</p>}
+            {value.markdown && (
+              <div className="unset-all">
+                <Markdown>{value.markdown}</Markdown>
+              </div>
+            )}
+          </div>
+        ))}
+      </Timeline>
+    </div>
   );
 }
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,15 +1,51 @@
 "use client";
 
-import { memo } from "react";
+import React, { JSX, memo, PropsWithChildren } from "react";
+import classnames from "classnames";
 
 export interface TimelineProps {
-    stage: number;
+  stage: number;
+  // texts: string[];
 }
 
-function Timeline({ stage }: TimelineProps) {
-    return (
-        <div className="bg-blue-600 h-1 w-full"></div>
-    )
+function Timeline({ stage, children }: PropsWithChildren<TimelineProps>) {
+  return (
+    <div className="bg-blue-600 h-1 relative w-full px-12">
+      <div
+        className="absolute left-1/2 h-1 flex gap-96 items-center transition duration-1000"
+        style={{ translate: -stage * 400 }}
+      >
+        {React.Children.map(children, (child, idx) => {
+          const className = classnames({
+            "w-8": idx == stage,
+            "h-8": idx == stage,
+          });
+          console.log(idx, idx == stage);
+          const textClass = classnames({
+            "opacity-0": idx != stage,
+          });
+          return (
+            <div key={idx} className="relative -translate-x-1/2 w-4 h-4">
+              <div
+                className={
+                  "w-4 h-4 bg-blue-600 rounded-full transition-all duration-1000 absolute top-1/2 left-1/2 -translate-1/2 " +
+                  className
+                }
+              />
+              <div
+                className={
+                  "absolute top-10 left-1/2 -translate-x-1/2 duration-1000 w-fit " +
+                  textClass
+                }
+              >
+                {child}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default memo(Timeline);
