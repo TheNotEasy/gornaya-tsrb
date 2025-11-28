b5a25b65ba9282e8939fb11dfe33add808dc15e2
TheNotEasy <sasha.antonov092@gmail.com>
2025-11-28 19:04:22 +0900

    favicon and starting url-optimizing

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,28 +1,48 @@
 "use client";
 
 import Timeline from "@/src/components/timeline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import events from "./events.json";
 import Markdown from "react-markdown";
+import { useSearchParams, useRouter } from "next/navigation";
 
-export default function Home() {
-  const [stage, setStage] = useState(0);
+export default function TimelinePage() {
+  const params = useSearchParams();
+  const router = useRouter();
+  const billboardMode = !!params.get("billboardMode");
+
+  const [stage, setStage] = useState(parseInt(params.get("stage") || "0") || 0);
+
+  useEffect(() => {
+    if (!billboardMode) return;
+    const interval = setInterval(
+      () => setStage((stage + 1) % events.length),
+      10000
+    );
+    return () => clearInterval(interval);
+  }, [billboardMode]);
+
+  useEffect(() => {}, [stage]);
 
   return (
-    <div className="flex flex-col gap-12">
-      <div className="flex flex-col align-center justify-center px-12 pt-12">
+    <div className="flex flex-col gap-12 pb-[900px] pt-20">
+      {/* <div className="flex flex-col align-center justify-center px-12 pt-12">
         <h1 className="text-center text-3xl">Таймлайн</h1>
         {stage}
-      </div>
-      <div className="flex gap-5 mx-auto">
-        <button onClick={() => setStage(stage - 1)} className="">
-          Назад
-        </button>
-        <button onClick={() => setStage(stage + 1)} className="">
-          Вперед
-        </button>
-      </div>
+      </div> */}
+      {!billboardMode && (
+        <div className="flex gap-5 mx-auto">
+          <button onClick={() => setStage(Math.max(stage - 1, 0))} className="">
+            Назад
+          </button>
+          <button
+            onClick={() => setStage(Math.min(stage + 1, events.length - 1))}
+          >
+            Вперед
+          </button>
+        </div>
+      )}
       <Timeline stage={stage}>
         {events.map((value, idx) => (
           <div className="flex flex-col items-center gap-5 w-lg" key={idx}>
